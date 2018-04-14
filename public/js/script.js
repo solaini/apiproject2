73893f5fdde5aa8865d8353a9698199da9da1cfb
Alexander Coffin <alex@wolfflow.io>
2018-04-14 14:16:50 -0500

    First commit

---
+++ public/js/script.js
@@ -0,0 +1,8 @@
+// Drum Arrays
+let kicks = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let snares = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let hiHats = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+let rideCymbals = [false, false, false, false, false, false, false, false, false, false, false, false, false, false, false, false];
+
+const toggleDrum = (drum, )
+
